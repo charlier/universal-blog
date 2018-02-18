f041e943f21000fc366ba6cd0ebc4ea4c9646825
Charlie Rogers <charles.rogers@bbc.co.uk>
2018-02-18 00:28:13 +0000

    update pure component tests

--- test/client/utils/pureComponent.js
+++ test/client/utils/pureComponent.js
@@ -3,21 +3,23 @@ import pureComponent from '../../../src/client/utils/pureComponent';
 describe('Pure Component', () => {
   it('should not update when props remain the same', () => {
     const component = new pureComponent({ props: true });
-    expect(component.shouldComponentUpdate({ props: true }, {})).toBe(false);
+    expect(component.shouldComponentUpdate({ props: true }, {})).toBeFalsy();
   });
 
   it('should update on props changes', () => {
     const component = new pureComponent({ props: true });
-    expect(component.shouldComponentUpdate({ props: false }, {})).toBe(true);
+    expect(component.shouldComponentUpdate({ props: false }, {})).toBeTruthy();
   });
 
   it('should not update when state remains the same', () => {
-    const component = new pureComponent({ state: true });
-    expect(component.shouldComponentUpdate({ state: true }, {})).toBe(false);
+    const component = new pureComponent({});
+    component.setState({ state: true });
+    expect(component.shouldComponentUpdate({}, { state: true })).toBeFalsy();
   });
 
   it('should update on state changes', () => {
-    const component = new pureComponent({ state: true });
-    expect(component.shouldComponentUpdate({ state: false }, {})).toBe(true);
+    const component = new pureComponent({});
+    component.setState({ state: true });
+    expect(component.shouldComponentUpdate({}, { state: false })).toBeTruthy();
   });
 });
