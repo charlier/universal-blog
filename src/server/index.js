0f91ac7d3b2476b970fc5689c11b6619e67a652a
Charlie Rogers <charles.rogers@bbc.co.uk>
2018-05-05 13:03:42 +0100

    add a basic logger

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,7 @@
 import cluster from 'cluster';
 import express from 'express';
 import os from 'os';
+import morgan from 'morgan';
 
 import App from './app';
 
@@ -11,6 +12,8 @@ if (cluster.isMaster) {
 } else {
   const server = express();
   server.disable('x-powered-by');
+  const logLevel = process.env.NODE_ENV === 'production' ? 'combined' : 'dev';
+  server.use(morgan(logLevel));
   server.use(express.static('./dist/client'));
   const port = process.env.PORT || 3000;
   server.listen(port);
