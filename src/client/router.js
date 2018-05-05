e4f96c8a7cc84fe5fabf9f18187e4d3cde38c385
Charlie Rogers <charles.rogers@bbc.co.uk>
2018-05-05 01:28:57 +0100

    better names

--- src/client/router.js
+++ src/client/router.js
@@ -2,20 +2,20 @@ import ptr from 'path-to-regexp';
 import 'isomorphic-unfetch';
 
 const Router = (routes) => {
-  const parsedRoutes = routes.map((r) => {
+  const parsedRoutes = routes.map((route) => {
     const keys = [];
-    const re = ptr(r.path, keys);
+    const regex = ptr(route.path, keys);
     return {
-      ...r,
+      ...route,
       keys,
-      re
+      regex
     };
   });
 
   return {
     match(url) {
       for (const route of parsedRoutes) {
-        const match = route.re.exec(url);
+        const match = route.regex.exec(url);
         if (match) {
           const [url, ...values] = match;
           const params = route.keys.reduce((memo, key, index) => {
@@ -23,20 +23,21 @@ const Router = (routes) => {
             return memo;
           }, {});
 
-          return route.page().then((m) => {
+          return route.page().then((matched) => {
             let initialProps = null;
             if (typeof document !== 'undefined' && document.getElementById('root').getAttribute('data-props')) {
-              const props = JSON.parse(document.getElementById('root').getAttribute('data-props'));
+              const root = document.getElementById('root');
+              const props = JSON.parse(root.getAttribute('data-props'));
+              root.removeAttribute('data-props');
               initialProps = Promise.resolve(props);
-              document.getElementById('root').removeAttribute('data-props');
-            } else if (m.default && m.default.getInitialProps) {
-              initialProps = m.default.getInitialProps({ params });
+            } else if (matched.default && matched.default.getInitialProps) {
+              initialProps = matched.default.getInitialProps({ params });
             } else {
               initialProps = Promise.resolve({});
             }
             return initialProps.then((props) => ({
               chunkName: route.chunkName,
-              Page: m.default,
+              Page: matched.default,
               props: {
                 path: route.path,
                 url,
